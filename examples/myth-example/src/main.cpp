087cf1c39c20a77d82209c5c7f5900ad30ae26d1
Andrew Drozdov <andrew@mrdrozdov.com>
2017-06-02 13:09:39 -0400

    Forward and backward pass

--- examples/myth-example/src/main.cpp
+++ examples/myth-example/src/main.cpp
@@ -36,9 +36,12 @@ class Linear
         int outpDim;
         THFloatTensor *weight;
         THFloatTensor *bias;
+        THFloatTensor *gradWeight;
+        THFloatTensor *gradBias;
         Linear(int inpDim, int outpDim);
         Variable * forward(Variable *x);
-        Variable * backward();
+        Variable * backward(Variable *x, THFloatTensor *gradOutput);
+        void clear_grads();
         ~Linear();
 };
 
@@ -46,9 +49,18 @@ Linear::Linear(int inpDim, int outpDim) {
     this->inpDim = inpDim;
     this->outpDim = outpDim;
     this->weight = THFloatTensor_newWithSize2d(this->outpDim, this->inpDim);
-    this->bias = THFloatTensor_newWithSize1d(this->outpDim);
+    this->gradWeight = THFloatTensor_newWithSize2d(this->outpDim, this->inpDim);
+    this->bias = NULL;
+    this->gradBias = NULL;
 
-    THFloatTensor_zero(this->bias);
+    // this->bias = THFloatTensor_newWithSize1d(this->outpDim);
+    // this->gradBias = THFloatTensor_newWithSize1d(this->outpDim);
+    // THFloatTensor_zero(this->bias);
+}
+
+void Linear::clear_grads() {
+    THFloatTensor_zero(this->gradWeight);
+    // THFloatTensor_zero(this->gradBias);
 }
 
 Variable * Linear::forward(Variable *x) {
@@ -79,9 +91,46 @@ Variable * Linear::forward(Variable *x) {
     return result;
 }
 
-Variable * Linear::backward() {
-    // TODO
-    return NULL;
+Variable * Linear::backward(Variable *x, THFloatTensor *gradOutput) {
+    long batch_size = x->data->size[0];
+    THFloatTensor *input = x->data;
+    THFloatTensor *weight = this->weight;
+    THFloatTensor *bias = this->bias;
+    THNNState *state = NULL;
+    THFloatTensor *addBuffer = THFloatTensor_newWithSize1d(batch_size);
+    float scale = 1;
+
+    THFloatTensor *gradInput = THFloatTensor_newWithSize2d(batch_size, this->inpDim); // TODO
+    THFloatTensor_zero(gradInput);
+
+    THFloatTensor *gradWeight = this->gradWeight; // TODO
+    THFloatTensor *gradBias = this->gradBias; // TODO
+
+    printf("updateGradInput\n");
+    // Calculate gradient with respect to input.
+    THNN_FloatLinear_updateGradInput(
+          state,
+          input,
+          gradOutput,
+          gradInput,
+          weight);
+
+    printf("accGradParameters\n");
+    // Done for layers that have parameters.
+    THNN_FloatLinear_accGradParameters(
+          state,
+          input,
+          gradOutput,
+          gradInput,
+          weight,
+          bias,
+          gradWeight,
+          gradBias,
+          addBuffer,
+          scale);
+
+    Variable *result = new Variable(gradInput);
+    return result;
 }
 
 void readFloat(THFile *file, THFloatTensor *tensor) {
@@ -105,6 +154,7 @@ DEFINE_string(weight_file, "w", "Data file");
 DEFINE_int32(batch_size, 2, "Data dim");
 DEFINE_int32(inp_dim, 10, "Data dim");
 DEFINE_int32(outp_dim, 10, "Data dim");
+DEFINE_double(learning_rate, 0.001, "Data dim");
 
 int main(int argc, char *argv[])
 {
@@ -120,12 +170,31 @@ int main(int argc, char *argv[])
     THFloatTensor *batch = THFloatTensor_newWithSize2d(batch_size, inp_dim);
     Linear *linear = new Linear(inp_dim, outp_dim);
 
+    // Initialization
     readFloat(batch_file, batch);
     readFloat(weight_file, linear->weight);
+    printf("weight: %f\n", THFloatTensor_sumall(linear->weight));
+    printf("weight[0]: %f\n", THFloatTensor_data(linear->weight)[0]);
 
+    // Forward Pass
     Variable *batch_var = new Variable(batch);
     Variable *outp = linear->forward(batch_var);
-    printf("%f\n", THFloatTensor_sumall(outp->data));
+    printf("forward: %f\n", THFloatTensor_sumall(outp->data));
+
+    // Backward Pass
+    THFloatTensor *loss = THFloatTensor_newWithSize2d(batch_size, outp_dim);
+    THFloatTensor_fill(loss, 1.0);
+    printf("loss: %f\n", THFloatTensor_sumall(loss));
+    linear->clear_grads();
+    printf("grads: %f\n", THFloatTensor_sumall(linear->gradWeight));
+    Variable *gradInput = linear->backward(outp, loss);
+    printf("gradInput: %f\n", THFloatTensor_sumall(gradInput->data));
+    printf("grads: %f\n", THFloatTensor_sumall(linear->gradWeight));
+    printf("grads[0]: %f\n", THFloatTensor_data(linear->gradWeight)[0]);
+
+    THFloatTensor_csub(linear->weight, linear->weight, FLAGS_learning_rate, linear->gradWeight);
+    printf("weight: %f\n", THFloatTensor_sumall(linear->weight));
+    printf("weight[0]: %f\n", THFloatTensor_data(linear->weight)[0]);
 
     THFloatTensor_free(batch);
     THFile_free(batch_file);
